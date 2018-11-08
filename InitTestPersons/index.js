a0545e48f4c88e7188106fc7ce270b8d8eee6a50
Lasse Haverinen <lasse.haverinen@oamk.fi>
2018-11-08 12:46:27 +0200

    Added functionality to add person faces and train the face recognizer

--- InitTestPersons/index.js
+++ InitTestPersons/index.js
@@ -1,30 +1,27 @@
 const axios = require('axios');
-const subscriptionKey = 'fill-this';
 
-async function createPerson(context, personGroupId, personData)
+const axiosCustom = axios.create({
+    baseURL: 'https://northeurope.api.cognitive.microsoft.com/face/v1.0',    
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': process.env['FaceApiAccessKey']         
+    }
+  });
+
+async function deletePersonGroup(personGroupId)
 {
     let returnData = {
         status: null,
         data: null
     };
 
-    // Create person group person
-    // https://northeurope.api.cognitive.microsoft.com/face/v1.0/persongroups/{personGroupId}/persons
+    // https://[location].api.cognitive.microsoft.com/face/v1.0/persongroups/{personGroupId}
 
     try {
-        const personCreateResponse = await axios.post(
-            `https://northeurope.api.cognitive.microsoft.com/face/v1.0/persongroups/${personGroupId}/persons/`,
-            personData,
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Ocp-Apim-Subscription-Key': subscriptionKey
-                }
-            });
+        const response = await axiosCustom.delete(`/persongroups/${personGroupId}`);
 
-        returnData.status = personCreateResponse.status;
-        returnData.data = personCreateResponse.data;
-
+        returnData.status = response.status;
+        returnData.data = response.data;
     }
     catch(error)
     {
@@ -33,9 +30,92 @@ async function createPerson(context, personGroupId, personData)
     }
 
     return returnData;
-
 }
 
+async function createPersonGroup(personGroupId)
+{
+    let returnData = {
+        status: null,
+        data: null
+    };
+
+    try {
+        const response = await axiosCustom.put(`/persongroups/${personGroupId}`,
+            {
+                name: "Test group 1"
+            });
+
+        returnData.status = response.status;
+        returnData.data = response.data;
+    }
+    catch(error)
+    {
+        returnData.status = error.response.status;
+        returnData.data = error.response.data;
+    }
+    return returnData;
+}
+
+async function createPerson(personGroupId, personData)
+{
+    let returnData = {
+        status: null,
+        data: null
+    };
+    try {
+        const personCreateResponse = await axiosCustom.post(`/persongroups/${personGroupId}/persons/`, personData);
+        returnData.status = personCreateResponse.status;
+        returnData.data = personCreateResponse.data;
+    }
+    catch(error)
+    {
+        returnData.status = error.response.status;
+        returnData.data = error.response.data;
+    }
+    return returnData;
+}
+
+async function addFaceToPerson(personGroupId, personId, imageData)
+{
+    let returnData = {
+        status: null,
+        data: null
+    };
+    try {
+        // https://northeurope.api.cognitive.microsoft.com/face/v1.0/persongroups/{personGroupId}/persons/{personId}/persistedFaces[?userData][&targetFace]
+        const response = await axiosCustom.post(`/persongroups/${personGroupId}/persons/${personId}/persistedFaces`,
+            {
+                "url": imageData
+            });
+        returnData.status = response.status;
+        returnData.data = response.data;
+    }
+    catch(error)
+    {
+        returnData.status = error.response.status;
+        returnData.data = error.response.data;
+    }
+    return returnData;
+}
+
+async function trainPersonGroup(personGroupId)
+{
+    let returnData = {
+        status: null,
+        data: null
+    };
+    try {        
+        const response = await axiosCustom.post(`/persongroups/${personGroupId}/train`);
+        returnData.status = response.status;
+        returnData.data = response.data;
+    }
+    catch(error)
+    {
+        returnData.status = error.response.status;
+        returnData.data = error.response.data;
+    }
+    return returnData;
+}
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
@@ -53,47 +133,54 @@ module.exports = async function (context, req) {
     const personGroupId = '07112018testgroup';
 
 
-    try {
-        const personGroupResponse = await axios.put(
-            'https://northeurope.api.cognitive.microsoft.com/face/v1.0/persongroups/' + personGroupId,
-            {
-                name: "Test group 1"
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Ocp-Apim-Subscription-Key': subscriptionKey }
-            });
+    // First delete possible existing person group
+    const deletePersonGroupResponse = await deletePersonGroup(personGroupId);
+    context.log("Person group delete status " + deletePersonGroupResponse.status);
+    context.log("Person group delete data " + deletePersonGroupResponse.data);
+   
+    const createPersonGroupResponse = await createPersonGroup(personGroupId);
+    context.log("Person group create status " + createPersonGroupResponse.status);
+    context.log("Person group create data " + createPersonGroupResponse.data);
 
-    } catch (error) {
-        context.log('PersonGroupResponse error');
-        context.log('Status ' + error.response.status);
-        context.log('Data ' + error.response.data);
-        if(error.response.status)
-        context.res = {
-            status: error.response.status,
-            body: error.response.data
-        };
-    }
-
-
-    const personCreateResponse = await createPerson(context, personGroupId, { name: 'Max Tester'});
-    if(personCreateResponse.status == 200)
-    {
-        context.log("Person created successfully");
-        context.log("Person id " + personCreateResponse.data.personId);
-    }
-
-    context.res = {
-        status: personCreateResponse.status,
-        body: personCreateResponse.data
+    const personCreateResponse = await createPerson(personGroupId, { name: 'Max Tester'});
+    context.log("Person create status " + personCreateResponse.status);
+    context.log("Person create data " + personCreateResponse.data);
+    
+    const faceData ={ 
+        person1: [
+            'Family1-Dad1.jpg',
+            'Family1-Dad2.jpg',
+            'Family1-Dad3.jpg',
+        ],
+        person2: [
+            'Family3-Lady1.jpg',
+            'Family3-Lady2.jpg',
+            'Family3-Lady4.jpg',
+        ],
+        person3: [
+            'Family3-Man1.jpg',
+            'Family3-Man2.jpg',
+            'Family3-Man3.jpg',
+        ]
     };
 
-    // Add face to person group person
-    // https://northeurope.api.cognitive.microsoft.com/face/v1.0/persongroups/{personGroupId}/persons/{personId}/persistedFaces[?userData][&targetFace]
+    for(let i = 0; i < faceData.person1.length; i++)
+    {
+        const filePath = process.env['TestImagePath'] + faceData.person1[i] + process.env['FileStorageSASKey'];
+        const addFaceResponse = await addFaceToPerson(personGroupId, personCreateResponse.data.personId, filePath);
+        context.log("Person 1 Face add status " + addFaceResponse.status);
+        context.log("Person 1 Face add data " + addFaceResponse.data);
+    }
 
-    // Train person group
-    // https://northeurope.api.cognitive.microsoft.com/face/v1.0/persongroups/{personGroupId}/train
+    const trainResponse = await trainPersonGroup(personGroupId);
+    context.log("Train status " + trainResponse.status);
+    context.log("Train data " + trainResponse.data);
+    
+    context.res = {
+        status: addFaceResponse.status,
+        body: addFaceResponse.data
+    };
+    
 
 
     // Detect face in Image
